2c14187104afc3c78e6084ad3ad01efa4aed854d
andres <gandres.ramirez@gmail.com>
2020-10-28 23:20:07 -0500

    Initial Logic

---
+++ main.lua
@@ -0,0 +1,130 @@
+push = require 'push'
+Class = require 'class'
+
+require 'Bird'
+
+require 'Pipe'
+
+--[[
+	constants definition
+]]--
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+--[[
+	Global Variables
+]]--
+local background = love.graphics.newImage('background.png')
+local backgroundScroll = 0
+
+local ground = love.graphics.newImage('ground.png')
+local groundScroll = 0
+
+-- parallax
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+local BACKGROUND_LOOPING_POINT = 413 --image boundary
+
+local bird = Bird()
+-- new table for pipes
+local pipes = {}
+
+local spawnTimer = 0
+
+--[[
+	Load function
+]]--
+function love.load()
+	love.graphics.setDefaultFilter('nearest', 'nearest')
+
+	love.window.setTitle('Fappy Bird ')
+--[[
+	Load window using push library
+]]--
+	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+		vsync=true,
+		fullScreen=false,
+		resizable=true
+	})
+	-- Init table to capture keys
+	love.keyboard.keysPressed = {}
+end
+
+--[[
+	Draw function
+]]--
+function love.draw()
+	push:start()
+	love.graphics.draw(background, -backgroundScroll, 0)
+	
+	for k, pipe in pairs(pipes) do
+		pipe:render()
+	end
+
+	love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+	bird:render()
+	push:finish()
+end
+
+--[[
+	Update function
+]]--
+function love.update(dt)
+	--Parallax effect
+	backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt)
+		% BACKGROUND_LOOPING_POINT
+	groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt)
+		% VIRTUAL_WIDTH
+
+	spawnTimer = spawnTimer + dt
+
+	if spawnTimer > 2 then
+		table.insert(pipes, Pipe())
+		spawnTimer = 0
+	end
+
+	bird:update(dt)
+
+	for k, pipe in pairs(pipes) do
+		pipe:update(dt)
+
+		if pipe.x < -pipe.width then
+			table.remove(pipes, k)
+		end
+	end
+
+	-- reset table of keys
+	love.keyboard.keysPressed = {}
+end
+
+--[[
+	Resize function
+]]--
+function love.resize(w, h)
+	push:resize(w, h)
+end
+
+--[[
+	Keyboard actions
+]]--
+function love.keypressed(key)
+	-- Detect when a key is captured
+	love.keyboard.keysPressed[key] = true
+
+	if key == 'escape' then
+		love.event.quit()
+	end
+end
+
+function love.keyboard.wasPressed(key)
+	if love.keyboard.keysPressed[key] then
+		return true
+	else
+		return false
+	end
+end
+
